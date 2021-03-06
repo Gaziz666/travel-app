9fbdaaf8f41cac168c1e2927872a67a2f055fed9
Roman Kohutyak <romankohutyak@gmail.com>
2021-03-06 10:37:08 +0200

    feat: add footer component

--- travel-app/src/components/App/App.tsx
+++ travel-app/src/components/App/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import MainPage from '../pages/main-page';
 import CountryPage from '../pages/country-page';
 import Header from '../header/header';
+import Footer from '../footer/footer';
 
 const App: React.FC = () => {
   return (
@@ -11,12 +12,13 @@ const App: React.FC = () => {
       <div className={classes.app}>
         <div className={classes.app__container}>
           <Header />
-          <main>
+          <main className={classes.app__main}>
             <Switch>
               <Route path="/" component={MainPage} exact />
               <Route path="/country" component={CountryPage} />
             </Switch>
           </main>
+          <Footer />
         </div>
       </div>
     </Router>
