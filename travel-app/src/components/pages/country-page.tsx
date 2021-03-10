67e443938ab74aff9980e0bec7e8a5253ce9465d
Roman Kohutyak <romankohutyak@gmail.com>
2021-03-10 09:57:41 +0200

    feat: tabs component

--- travel-app/src/components/pages/country-page.tsx
+++ travel-app/src/components/pages/country-page.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import CenteredTabs from '../tabs/tabs';
 
 const CountryPage: React.FC = () => {
-  return <div>Country Page</div>;
+  return (
+    <React.Fragment>
+      <CenteredTabs />
+    </React.Fragment>
+  );
 };
 
 export default CountryPage;
