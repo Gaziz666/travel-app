e343379e5a80fba7e4fe90ad3f44a4d17f87916f
juliememe <julie_meme@mail.ru>
2021-03-11 09:27:26 +0400

    feat: add styles

--- travel-app/src/components/pages/country-page.tsx
+++ travel-app/src/components/pages/country-page.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import CountryInfo from '../country-info/country-info';
 
 const CountryPage: React.FC = () => {
-  return <div>Country Page</div>;
+  // return <div>Country Page</div>;
+  return(
+    <CountryInfo/>
+  )
 };
 
 export default CountryPage;
