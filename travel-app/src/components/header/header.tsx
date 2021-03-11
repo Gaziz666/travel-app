14b43a51e9b9883d49a7b7cb4ba129ae4e6dd75d
Roman Kohutyak <romankohutyak@gmail.com>
2021-03-11 19:25:18 +0200

    feat: update footer and header styles

--- travel-app/src/components/header/header.tsx
+++ travel-app/src/components/header/header.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import classes from './header.module.css';
 import { Link } from 'react-router-dom';
 import LanguageMenu from '../LanguageMenu/Language-menu';
+import { ReactComponent as UserSvg } from '../../assets/images/user.svg';
 
 const Header: React.FC = () => {
   return (
-    <div className={classes.header}>
+    <header className={classes.header}>
       <div className={classes.header__logo}>
         <Link to="/" className={classes['header__logo-icon']}></Link>
         <span className={classes['header__logo-text']}>Explore the World</span>
@@ -18,9 +19,11 @@ const Header: React.FC = () => {
           Destinations
         </Link>
         <LanguageMenu />
-        <Link to="/" className={classes.header__login}></Link>
+        <Link to="/" className={classes.header__login}>
+          <UserSvg fill="#3ccdd7" className={classes.header__login_svg} />
+        </Link>
       </div>
-    </div>
+    </header>
   );
 };
 
