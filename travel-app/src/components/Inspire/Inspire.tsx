6663730f391cc868db964c72be7d98a9e72b8ac2
Roman Kohutyak <romankohutyak@gmail.com>
2021-03-12 23:21:25 +0200

    feat: video player

--- travel-app/src/components/Inspire/Inspire.tsx
+++ travel-app/src/components/Inspire/Inspire.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Player, BigPlayButton } from 'video-react';
+import classes from './inspire.module.css';
 
 const Inspire: React.FC = () => {
-  return <div>Inspire</div>;
+  return (
+    <div className={classes.player__wrapper}>
+      <Player
+        playsInline
+        poster="/assets/poster.png"
+        src="../../assets/video/short-track.mp4"
+      >
+        <BigPlayButton position="center" />
+      </Player>
+    </div>
+  );
 };
 
 export default Inspire;
